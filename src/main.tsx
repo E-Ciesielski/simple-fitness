30ed0ab2c50ee0f163cfb64bd38107eb58f87b86
Eryk <eryk0399@gmail.com>
2025-11-16 16:27:22 +0100

    create mockup layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import DashboardPage from "./dashboard/DashboardPage.tsx";
+import CaloriesPage from "./calories/CaloriesPage.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route index element={<DashboardPage />} />
+          <Route path="calories" element={<CaloriesPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
